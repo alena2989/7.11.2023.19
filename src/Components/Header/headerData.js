b07b7e8403500e22f58c325216dffe7f158247bb
alena2989 <128296892+alena2989@users.noreply.github.com>
2023-11-07 17:54:25 +1000

    Initial commit

---
+++ src/Components/Header/headerData.js
@@ -0,0 +1,6 @@
+export const headerData = [
+  { name: "Home", link: "/home" },
+  { name: "shop", link: "/shop" },
+  { name: "Contact", link: "/contact" },
+  { name: "About", link: "/About" }
+];
